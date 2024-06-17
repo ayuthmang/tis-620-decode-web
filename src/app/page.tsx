42fb6baa03369a6832f46e0a6a446cb3390b6f19
Ayuth Mangmesap <ayuth.mang@gmail.com>
2024-06-18 00:04:01 +0700

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,32 @@
 "use client";
-import {
-  Flex, Container,
-  Box
-} from "@radix-ui/themes";
-import { useFormik, Form, Formik } from "formik";
-import { z } from "zod";
+import { Flex, Container, Box } from "@radix-ui/themes";
 import { useTis620 } from "@/hooks/use-tis-620";
-import { toFormikValidationSchema } from "zod-formik-adapter";
 import * as Label from "@radix-ui/react-label";
 import Header from "@/components/Header";
 import TextArea from "@/components/TextArea";
 import Footer from "@/components/Footer";
+import React from "react";
+import { useFormState } from "react-dom";
 
-const EditorSchema = z.object({
-  input: z.string(),
-  output: z.string(),
-});
-
-type EditorValues = z.infer<typeof EditorSchema>;
-
-const initialEditorValues: EditorValues = {
-  input: "",
-  output: "",
-};
+function useTextArea() {
+  const [value, setValue] = React.useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+  };
+  return { value, handleChange };
+}
 
 export default function Home() {
-  const formik = useFormik<EditorValues>({
-    validationSchema: toFormikValidationSchema(EditorSchema),
-    initialValues: initialEditorValues,
-    onSubmit: handleSubmit,
-  });
   const { tis620ToUtf8, utf8ToTis620 } = useTis620();
-
-  function handleSubmit(values: EditorValues) {
-    console.log(values);
-  }
+  const inputTextArea = useTextArea();
+  const outputTextArea = useTextArea();
 
   function handleOnInputChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
     const inputVal = e.target.value;
-    formik.setValues({
-      input: inputVal,
-      output: tis620ToUtf8(inputVal),
-    });
+    // formik.setValues({
+    //   input: inputVal,
+    //   output: tis620ToUtf8(inputVal),
+    // });
   }
 
   function handleOnOutputChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
@@ -57,40 +42,35 @@ export default function Home() {
       <Header />
       <Flex display="flex" align="stretch" height="100%">
         <Container size="4" p="4" height="100%">
-          <Formik<EditorValues>
-            initialValues={initialEditorValues}
-            onSubmit={handleSubmit}
-          >
-            <Box asChild height={"100%"}>
-              <Form onSubmit={formik.handleSubmit} onReset={formik.handleReset}>
-                <Flex gap="4" direction="row" height="100%">
-                  <Flex direction="column" gap="2" flexGrow={"1"}>
-                    <Label.Root htmlFor="input">TIS-620 Input</Label.Root>
-                    <TextArea
-                      id="input"
-                      name="input"
-                      placeholder="Type here..."
-                      onChange={handleOnInputChange}
-                      value={formik.values.input}
-                      spellCheck={false}
-                      autoFocus
-                    />
-                  </Flex>
-                  <Flex direction="column" gap="2" flexGrow={"1"}>
-                    <Label.Root htmlFor="output">UTF-8 Output</Label.Root>
-                    <TextArea
-                      id="output"
-                      name="output"
-                      placeholder="Type here..."
-                      onChange={handleOnOutputChange}
-                      value={formik.values.output}
-                      spellCheck={false}
-                    />
-                  </Flex>
+          <Box asChild height={"100%"}>
+            <form>
+              <Flex gap="4" direction="row" height="100%">
+                <Flex direction="column" gap="2" flexGrow={"1"}>
+                  <Label.Root htmlFor="input">TIS-620 Input</Label.Root>
+                  <TextArea
+                    id="input"
+                    name="input"
+                    placeholder="Example: ÊÇÑÊ´Õ"
+                    onChange={inputTextArea.handleChange}
+                    value={inputTextArea.value}
+                    spellCheck={false}
+                    autoFocus
+                  />
                 </Flex>
-              </Form>
-            </Box>
-          </Formik>
+                <Flex direction="column" gap="2" flexGrow={"1"}>
+                  <Label.Root htmlFor="output">UTF-8 Output</Label.Root>
+                  <TextArea
+                    id="output"
+                    name="output"
+                    placeholder="สวัสดี"
+                    onChange={outputTextArea.handleChange}
+                    value={outputTextArea.value}
+                    spellCheck={false}
+                  />
+                </Flex>
+              </Flex>
+            </form>
+          </Box>
         </Container>
       </Flex>
       <Footer />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
 import styled from "@emotion/styled";
-import { Container, Flex, Link } from "@radix-ui/themes";
+import { Box, Container, Flex, Link } from "@radix-ui/themes";
+import { GitHubIcon } from "./Icons";
+import { remToPx } from "@/utils/pixel-to-rem";
 
 export default function Header() {
   return (
     <Nav>
       <Container p="4">
-        <Flex align="center" justify={"center"}>
+        <Flex align="center" justify="center">
           <Flex flexGrow="1" />
-          <Link href="https://github.com/ayuthmagn/tis-620-decode-web">
-            GitHub
+          <Link href="https://github.com/ayuthmang/tis-620-decode-web">
+            <Flex gapX={"4"}>
+              <GitHubIcon width={remToPx(2)} height={remToPx(2)} />
+              GitHub
+            </Flex>
           </Link>
         </Flex>
       </Container>
@@ -17,5 +22,5 @@ export default function Header() {
 }
 
 const Nav = styled.nav`
-  height: 60px;
+  height: var(--header-height);
 `;
